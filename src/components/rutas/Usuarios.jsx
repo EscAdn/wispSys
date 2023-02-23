5c0a1c1762789e27294f4c1b7bc3884bac7e375a
EscAdn <adan.imh@gmail.com>
2023-02-23 10:41:35 -0600

    Prueba commit

--- src/components/rutas/Usuarios.jsx
+++ src/components/rutas/Usuarios.jsx
@@ -1,9 +1,11 @@
 import Header from "../extras/Header";
+import Layout from "../extras/Layout";
 
 export const Usuarios = () => {
   return (
     <>
       <Header title="Usuarios" />
+      <Layout />
     </>
   );
 };
