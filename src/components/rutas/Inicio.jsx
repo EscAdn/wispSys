54756e6731fc3fd3a8923708a5c1dcdbe521f41f
EscAdn <adan.imh@gmail.com>
2023-03-12 16:44:58 -0600

    Tabla de facturas reparada

--- src/components/rutas/Inicio.jsx
+++ src/components/rutas/Inicio.jsx
@@ -4,20 +4,21 @@ import { helpHttp } from "../../helpers/helpHttp";
 import { urls } from "../../utils/endpoints";
 
 import Header from "../extras/Header";
-import Invoices from "./Invoices";
 import Layout from "./../extras/Layout";
 import Card from "./../extras/Card";
 import Message from "../extras/Message";
+import Table from "./Invoices/Table";
+import Loading from "../extras/Loading";
 
 export const Inicio = () => {
-  const [db, setDb] = useState([]);
+  const [db, setDb] = useState(null);
   const [error, setError] = useState(null);
 
   const getData = async () => {
     const resp = await helpHttp().get(urls.url_invoices);
     if (resp.err) {
       setError(resp);
-      setDb([]);
+      setDb(null);
     } else {
       setError(null);
       setDb(resp);
@@ -28,15 +29,27 @@ export const Inicio = () => {
     getData();
   }, []);
 
+  if (error) {
+    return (
+      <Card>
+        <Message msg={`${error.status}: ${error.statusText}`} />
+      </Card>
+    );
+  }
+
   return (
     <>
       <Header title="Inicio" />
-      <Layout>
-        <Card md="col-md-3">Tareas Pendientes</Card>
-        <Card md="col-md-9">
-          <Invoices db={db} />
-        </Card>
-      </Layout>
+      {db ? (
+        <Layout>
+          <Card md="col-md-3">Tareas Pendientes</Card>
+          <Card md="col-md-9">
+            <Table data={db} setDataToEdit={undefined} />
+          </Card>
+        </Layout>
+      ) : (
+        <Loading />
+      )}
     </>
   );
 };
--- src/components/rutas/Invoices/Table.jsx
+++ src/components/rutas/Invoices/Table.jsx
@@ -1,48 +1,47 @@
 const Table = ({ data }) => {
-  console.log(data);
+  const pagarFactura = (data) => {
+    alert("Pagar Factura " + data.id);
+  };
+
+  const anularFactura = (id) => {
+    alert("Anular Factura " + id);
+  };
+
   return (
-    <div className="mt-4 col-12 p-2 h-max scroll overflow-auto">
+    <>
       <div className="card-header bg-white col-12 p-2">
         <h5 className="fw-bold text-center">Facturas Pendientes</h5>
       </div>
-      <table className="table table-responsive table-hover shadow p-3 mb-2 bg-body">
-        <thead>
-          <tr className="bg-personalizado">
-            <th scope="col">#</th>
-            <th scope="col">Cliente</th>
-            <th scope="col">Fecha</th>
-            <th scope="col">Cantidad</th>
-            <th scope="col">Estado</th>
-            <th scope="col">Opciones</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.length === 0 ? (
-            <tr className="text-center">
-              <td colSpan="6">
-                <div className="spinner-grow text-primary" role="status">
-                  <span className="visually-hidden">Loading...</span>
-                </div>
-              </td>
+      <div className="mt-1 col-12 p-2 h-max scroll overflow-auto">
+        <table className="table table-responsive table-hover shadow p-3 mb-2 bg-body">
+          <thead>
+            <tr className="bg-personalizado">
+              <th scope="col">#</th>
+              <th scope="col">Cliente</th>
+              <th scope="col">Fecha</th>
+              <th scope="col">Cantidad</th>
+              <th scope="col">Estado</th>
+              <th scope="col">Opciones</th>
             </tr>
-          ) : (
-            data.map((x) => {
-              <tr key={x.id}>
-                <td className="h6">{x.id}</td>
-                <td className="h6 fw-bold">{x.client}</td>
-                <td className="h6">{x.from.substring(0, 10)}</td>
-                <td className="h6">$ {x.price} MNX</td>
-                <td className="h6">
-                  {/* {x.state === "Activa" ? (
-                    <span className="h6 badge bg-primary">{x.state}</span>
+          </thead>
+          <tbody>
+            {data.map((e) => (
+              <tr key={e.id}>
+                <td>{e.id}</td>
+                <td className="h6 fw-bold">{e.client}</td>
+                <td>{e.from.substring(0, 10)}</td>
+                <td>$ {e.price} MNX</td>
+                <td>
+                  {e.state === "Activa" ? (
+                    <span className="h6 badge bg-primary">{e.state}</span>
                   ) : (
-                    <span className="h6 badge bg-danger">{x.state}</span>
-                  )} */}
+                    <span className="h6 badge bg-danger">{e.state}</span>
+                  )}
                 </td>
                 <td>
                   <button
                     className="btn btn-sm btn-outline-wisp"
-                    onClick={() => {}}
+                    onClick={() => pagarFactura(e)}
                     title="Pagar"
                   >
                     <i className="fas fa-dollar"></i>
@@ -50,18 +49,18 @@ const Table = ({ data }) => {
                   &nbsp;
                   <button
                     className="btn btn-sm btn-outline-wisp"
-                    onClick={() => {}}
+                    onClick={() => anularFactura(e.id)}
                     title="Anular"
                   >
                     <i className="fas fa-warning"></i>
                   </button>
                 </td>
-              </tr>;
-            })
-          )}
-        </tbody>
-      </table>
-    </div>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 };
 
