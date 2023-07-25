717536477e29da9755be557fa54993cc3f726652
EscAdn <adan.imhmail.com>
2023-07-25 09:04:16 -0600

    Cambio desechado

--- src/components/rutas/Planes/Form.jsx
+++ src/components/rutas/Planes/Form.jsx
@@ -1,12 +1,12 @@
 import { useEffect, useState } from "react";
-import {Formik, Form} from 'formik'
-import * as Yub from 'yup'
+import { Formik, Form } from "formik";
+import * as Yub from "yup";
 
 // Components
 import ButtonsForm from "../../extras/ButtonsForm";
 import Input from "../../extras/Input";
 
-const initialForm = {
+let initialForm = {
   id: null,
   name: "",
   ceil_down_mbps: 0,
@@ -16,14 +16,13 @@ const initialForm = {
 };
 
 const Formulario = ({ createData, updateData, dataToEdit, setDataToEdit }) => {
-
-   // useEffect(() => {
-   //  if (dataToEdit) {
-   //    setForm(dataToEdit);
-   //  } else {
-   //    setForm(initialForm);
-   //  }
-   // }, [dataToEdit]);
+  // useEffect(() => {
+  //  if (dataToEdit) {
+  //    setForm(dataToEdit);
+  //  } else {
+  //    setForm(initialForm);
+  //  }
+  // }, [dataToEdit]);
 
   // const handleChange = (e) => {
   //   setForm({
@@ -56,63 +55,80 @@ const Formulario = ({ createData, updateData, dataToEdit, setDataToEdit }) => {
   // };
 
   const validate = Yub.object({
-    name: Yub.string()
-  })
+    name: Yub.string().required("Campo requerido"),
+  });
 
   const handleSubmit = (values, actions) => {
     // Aqui debo registrar los datos
-    console.log(values)
+    console.log(values);
+
+    setDataToEdit(!dataToEdit);
 
     // Si todo sale bien resetear el formulario
-    actions.resetForm()
-  }
+    actions.resetForm();
+  };
+
+  const editar = () => {
+    setDataToEdit(!dataToEdit);
+    initialForm = {
+      id: 12,
+      name: "Algun Nombre",
+      ceil_down_mbps: 10,
+      ceil_up_mbps: 20,
+      price: 1230,
+      contracts_count: 10,
+    };
+  };
 
   return (
-    <Formik className="col-sm-12" 
-      initialValues={initialForm} 
-      onSubmit={handleSubmit} 
-      onReset={() => {}} 
-      validationSchema={}>
-      <Form className="text-center p-4">
-        <h5 className="card-header bg-white">
-          <span className="h4 fw-bold">
-            {dataToEdit ? "Modificar" : "Registrar"}
-          </span>
-        </h5>
-        <div className="card-body">
-          <Input
-            label="Nombre"
-            name="name"
-            type="text"
-          />
-          <Input
-            label="Max. Subida (Mbps)"
-            name="ceil_up_mbps"
-            type="number"
-            step="1"
-            min="1"
-            max="100"
-          />
-          <Input
-            label="Max. Descargar (Mbps)"
-            name="ceil_down_mbps"
-            type="number"
-            step="1"
-            min="1"
-            max="100"
-          />
-          <Input
-            label="Precio (MNX)"
-            name="price"
-            type="number"
-            step="50"
-            min="100"
-            max="1000"
-          />
-          <ButtonsForm dataToEdit={dataToEdit} />
-        </div>
-      </Form>
-    </Formik>
+    <>
+      <button className="btn btn-sm btn-danger" onClick={editar}>
+        Editar
+      </button>
+      <Formik
+        className="col-sm-12"
+        initialValues={initialForm}
+        onSubmit={handleSubmit}
+        onReset={() => {}}
+        validationSchema={validate}
+      >
+        <Form className="text-center p-4">
+          <h5 className="card-header bg-white">
+            <span className="h4 fw-bold">
+              {dataToEdit ? "Modificar" : "Registrar"}
+            </span>
+          </h5>
+          <div className="card-body">
+            <Input label="Nombre" name="name" type="text" />
+            <Input
+              label="Max. Subida (Mbps)"
+              name="ceil_up_mbps"
+              type="number"
+              step="1"
+              min="1"
+              max="100"
+            />
+            <Input
+              label="Max. Descargar (Mbps)"
+              name="ceil_down_mbps"
+              type="number"
+              step="1"
+              min="1"
+              max="100"
+            />
+            <Input
+              label="Precio (MNX)"
+              name="price"
+              type="number"
+              step="50"
+              min="100"
+              max="1000"
+            />
+            <ButtonsForm dataToEdit={dataToEdit} />
+          </div>
+        </Form>
+      </Formik>
+    </>
   );
 };
 
